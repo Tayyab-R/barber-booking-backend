21318e6602a10e5f577582d517dd3f808d83a1a4
Tayyab-R <riaztayyab212@gmail.com>
2024-12-24 16:41:29 +0500

    added TimeStamp model for timestamps for models. imported RolesChoices enum for choices in CustomUser model

--- user/models.py
+++ user/models.py
@@ -2,44 +2,47 @@ from typing import Collection, Iterable
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils import timezone
-from django.core.exceptions import ValidationError, NON_FIELD_ERRORS
+from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
 from mysite import settings
 
 from .manager import CustomUserManager
+from .utils import RolesChoices
 
-class CustomUser(AbstractUser):
-    ROLES =(
-        ('CUSTOMER', 'Customer'),
-        ('BARBER', 'Barber'),
-        ('ADMIN', 'Admin'),
-    )
 
+class TimeStampModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class CustomUser(AbstractUser, TimeStampModel):
     username = None
     email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=20, unique=True, null=True)
-    role = models.CharField(max_length=50, choices=ROLES, default='CUSTOMER')
+    role = models.CharField(max_length=50, choices=RolesChoices.choices, default=RolesChoices.CUSTOMER.value)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = CustomUserManager()
+objects = CustomUserManager()
 
-    def __str__(self) -> str:
-        return self.email
+def __str__(self) -> str:
+    return self.email
 
     
-class BarberProfile(models.Model):
+class BarberProfile(TimeStampModel):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='barber_profile')
     is_available = models.BooleanField(default=False)
+        
     def __str__(self) -> str:
         return self.user.email
     
-class Slots(models.Model):
-    barber = models.ForeignKey(BarberProfile, on_delete=models.CASCADE, related_name='slots')    
+class Slots(TimeStampModel):
     start_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
-    customer = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.CASCADE, related_name='slot')
     is_booked = models.BooleanField(default=False)
     
     def __str__(self) -> str:
@@ -58,20 +61,18 @@ class Slots(models.Model):
         return super(Slots, self).save(*args, **kwargs)
 
     
-class Review(models.Model):
+class Review(TimeStampModel):
     review = models.TextField()
     barber = models.ForeignKey(BarberProfile, related_name='reviews', on_delete=models.CASCADE)
     slot = models.ForeignKey(Slots, related_name='reviews', on_delete=models.CASCADE)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='reviews', null=True, on_delete=models.SET_NULL)
-    created_at = models.DateTimeField(auto_now=True)
 
 
-class Money(models.Model):
+class Money(TimeStampModel):
     amount = models.PositiveIntegerField()
     barber = models.ForeignKey(BarberProfile, related_name='money', on_delete=models.CASCADE)
     barber_slot = models.ForeignKey(Slots, related_name='amount', on_delete=models.CASCADE)
     customer = models.ForeignKey(settings.AUTH_USER_MODEL, related_name='paid', on_delete=models.CASCADE)
-    paid_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self) -> str:
         return str(self.amount)
