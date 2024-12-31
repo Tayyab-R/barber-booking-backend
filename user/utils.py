60e0fb72d630b42ebccc8c939f8fd408017f823f
Tayyab-R <riaztayyab212@gmail.com>
2024-12-31 19:13:56 +0500

    added a function 'CreateSlots' to create slots

--- user/utils.py
+++ user/utils.py
@@ -1,5 +1,9 @@
 from enum import Enum
+import datetime
 
+from django.utils import timezone
+
+from . import models
 class RolesChoices(Enum):
     BARBER = 'Barber'
     CUSTOMER = 'Customer'
@@ -8,3 +12,14 @@ class RolesChoices(Enum):
     @classmethod
     def choices(cls):
         return [(key.value, key.name) for key in cls]
+
+    
+def CreateSlots(barber):
+    hours = 12
+    today = datetime.datetime.today()
+    for hour in range(hours):
+        start_time = datetime.datetime(today.year, today.month, today.day, hour=hour, minute=0, tzinfo=timezone.timezone.utc)
+        end_time = datetime.datetime(today.year, today.month, today.day, hour=hour+1, minute=0, tzinfo=timezone.timezone.utc)
+
+        slot = models.Slots.objects.create(barber=barber, start_time=start_time, end_time=end_time)
+        slot.save()
