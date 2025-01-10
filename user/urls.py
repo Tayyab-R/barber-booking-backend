097b38d8ad0295f72c5f2d976a62e4c42da7ef87
Tayyab-R <riaztayyab212@gmail.com>
2025-01-10 11:53:17 +0500

    added urls to book, cancel and delete bookings

--- user/urls.py
+++ user/urls.py
@@ -13,8 +13,9 @@ urlpatterns = [
     path('barber/signup/', views.CreateBarberProfile),
     path('profile/', views.Profile),
     path('barber/slots/', views.ListBarberSlots),
-    path('barber/slots/<int:pk>/', views.BookBarberSlot),
+    path('barber/slots/book/<int:pk>/', views.BookBarberSlot),
+    path('barber/slots/cancel/<int:pk>/', views.BookBarberSlot),
+    path('barber/slots/delete/<int:pk>/', views.BookBarberSlot),
     path('barber/write-review/<int:pk>/', views.WriteReviewOfBarberView),
     path('barber/pay/<int:pk>/', views.PayMoneyToBarberView),
-
 ]
